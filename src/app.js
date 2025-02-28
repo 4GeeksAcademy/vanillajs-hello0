c531ccfaffa7966191e85f68c0dd21cdb02f1798
raulbordon <raulbordon2002@hotmail.es>
2025-02-28 11:19:45 +0000

    arreglos

--- src/app.js
+++ src/app.js
@@ -1,21 +1,11 @@
-import "bootstrap";
-import "./style.css";
+let pronoun = ['El', 'Tu', 'Mi'];
+let adj = ['increible', 'pequeño', 'gran' ];
+let noun = ['perro', 'rata', 'gato', 'pantalon', 'coche'];
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  let pronoun = ['the', 'our', 'his'];
-  let adj = ['great', 'big', 'amazing' ];
-  let noun = ['jogger', 'racoon', 'dog', 'merchant', 'driver'];
-
-  for(let i = 0; i < pronoun.length; i++) {
-    for(let j = 0; j < adj.length; j++) {
-      for(let k = 0; k < noun.length; k++) {
-        console.log(pronoun[i] + adj[j] + noun[k]+'.com'); 
-      }
+for(let i = 0; i < pronoun.length; i++) {
+  for(let j = 0; j < adj.length; j++) {
+    for(let k = 0; k < noun.length; k++) {
+      console.log(pronoun[i] + adj[j] + noun[k]+'.com'); 
     }
   }
 }
